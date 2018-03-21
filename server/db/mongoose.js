782669484c1d4093484210da1d143c97139971de
Vinicius Duarte <vniciusdiniz@gmail.com>
2018-03-21 14:56:02 +1100

    mongoose connect test One

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+
+mongoose.connect('mongodb://vini:pass@ds061721.mlab.com:61721/todos');
+//mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
