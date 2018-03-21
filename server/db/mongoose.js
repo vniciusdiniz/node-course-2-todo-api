8cd8b556be5d53271bfc4f73b8ad19e635ec1233
Vinicius Duarte <vniciusdiniz@gmail.com>
2018-03-21 15:09:43 +1100

    mongoose connect test Two

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,9 @@ var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://vini:pass@ds061721.mlab.com:61721/todos');
-//mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
-
+//mongoose.connect('mongodb://vini:pass@ds061721.mlab.com:61721/todos');
+console.log("URL HERE");
+ console.log(process.env.DATABASE_URL);
+mongoose.connect( process.env.DATABASE_URL);
+// || 'mongodb://localhost:27017/TodoApp'
 module.exports = {mongoose};
