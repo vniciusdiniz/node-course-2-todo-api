3d2642fda4d3cfce1bf0e4149e4cd9cc3b7a4860
Vinicius Duarte <vniciusdiniz@gmail.com>
2018-03-21 12:44:53 +1100

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ var bodyParser  = require('body-parser');
 var {mongoose}  = require('./db/mongoose');
 var {Todo}      = require('./models/todo');
 var {User}      = require('./models/user');
+var {ObjectID}  = require('mongodb');
 
 var app         = express();
 
@@ -31,7 +32,27 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     });
-    
+
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if ( ObjectID.isValid(id) ){
+        console.log("ok")
+    }else {
+        console.log("Invalid ID");
+        res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo){
+            return res.status(404).send();
+        }
+        res.send(todo);
+    }, (e) => {
+        res.status(400).send();
+    });
+
 });
 
 app.listen(3000, () => {
