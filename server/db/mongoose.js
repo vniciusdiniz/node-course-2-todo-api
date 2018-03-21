17c6f488b544ceda64fd3bd1a79cc2b17952692d
Vinicius Duarte <vniciusdiniz@gmail.com>
2018-03-21 15:25:26 +1100

    add connection to mlab via env.DATABASE_URL

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,9 +2,6 @@ var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 
-//mongoose.connect('mongodb://vini:pass@ds061721.mlab.com:61721/todos');
-console.log("URL HERE");
- console.log(process.env.DATABASE_URL);
-mongoose.connect( process.env.DATABASE_URL);
-// || 'mongodb://localhost:27017/TodoApp'
+mongoose.connect( process.env.DATABASE_URL || 'mongodb://localhost:27017/TodoApp');
+
 module.exports = {mongoose};
